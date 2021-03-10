42d4d9fbf00efe9528c26dd86820db43b86792e9
Javier Enríquez <javierem_94@ciencias.unam.mx>
2021-03-10 17:22:47 -0600

    Delete env.sh

--- config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='development'
-export PORT=3000
-export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
-export MONGODB_URI='mongodb+srv://<username>:<password>@cluster0-xmea4.mongodb.net/adoptapet?retryWrites=true&w=majority'
