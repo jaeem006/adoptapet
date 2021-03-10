2fffc097857ad30a485ba4fa20e42b07a591fb55
jaeem006 <javierem.94@gmail.com>
2021-03-10 17:17:48 -0600

    first commit

---
+++ config/env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV='development'
+export PORT=3000
+export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
+export MONGODB_URI='mongodb+srv://<username>:<password>@cluster0-xmea4.mongodb.net/adoptapet?retryWrites=true&w=majority'
